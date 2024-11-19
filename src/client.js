298bfddece7278f78422f48b6e589b2641215407
llynoirx <kathyho04@gmail.com>
2024-11-19 15:45:28 -0500

    add supabase

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import {createClient} from '@supabase/supabase-js'
+
+const URL = 'https://xptuyhlrruksyulsvlyn.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwdHV5aGxycnVrc3l1bHN2bHluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwNDgxNjIsImV4cCI6MjA0NzYyNDE2Mn0.HlrdypOgWX88tE2Z1uT70xUAB9c71JaTHV9vPmaJxEI';
+
+export const supabase = createClient(URL, API_KEY);
