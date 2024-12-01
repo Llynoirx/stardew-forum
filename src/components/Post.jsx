ea900d68d5c0108e0e015d36ce07ca28273229b9
Kathy Ho <kathyho@Kathys-MacBook-Air.local>
2024-12-01 17:51:33 -0500

    add new lines for posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ import './Post.css'
 import { Link } from 'react-router-dom'
 import more from '../assets/more.png'
 
-const Post = (props) =>  {
+const Post = (props) => {
 
   const timestamp = new Date(props.created_at)
   const currentTime = new Date()
@@ -13,16 +13,25 @@ const Post = (props) =>  {
 
   return (
       <div className="Post">
-        <div> <Link to={'edit/'+ props.id}><img className="moreButton" alt="edit button" src={more} /></Link> </div>
-          <Link to={'post/'+ props.id}><h2 className="name">{props.title}</h2></Link>
-          <div>{props.content}</div>
+        <div> 
+          <Link to={'edit/'+ props.id}>
+            <img className="moreButton" alt="edit button" src={more} />
+          </Link> 
+        </div>
+        <Link to={'post/'+ props.id}>
+          <h2 className="name">{props.title}</h2>
+        </Link>
+        
+        {/* Add the "content" class to preserve line breaks */}
+        <div className="content">{props.content}</div>
           
-          <div className="upvotes">
-            { diffMinutes < 60 ? <p>Posted {diffMinutes} minutes ago</p> 
-            : diffHours < 24 ? <p>Posted {diffHours} hours ago</p>
-            : <p>Posted {diffDays} days ago</p>
-            } <p>{props.upvotes} upvotes</p>
-          </div>
+        <div className="upvotes">
+          { diffMinutes < 60 ? <p>Posted {diffMinutes} minutes ago</p> 
+          : diffHours < 24 ? <p>Posted {diffHours} hours ago</p>
+          : <p>Posted {diffDays} days ago</p>
+          } 
+          <p>{props.upvotes} upvotes</p>
+        </div>
       </div>
   );
 };
