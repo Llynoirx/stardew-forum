44efd0c563a97e1bda19f1932ad70d104e20501c
Kathy Ho <kathyho04@gmail.com>
2024-12-02 08:52:50 -0500

    rotating loading screen

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,13 +1,17 @@
-import junimo from '../assets/junimo-dance.gif'
+import React from 'react';
+import junimo from '../assets/junimo-dance.gif'; // Importing the gif
+import './LoadingPage.css'; // Import the CSS file for styling
 
-const LoadingPage = () =>  {
-
+const LoadingPage = () => {
     return (
-        <div>
-            <img src={junimo} className="logo"/>
-            <p>Loading...</p>
+        <div className="loading-container">
+            {/* Mini container for loading content */}
+            <div className="loading-box">
+                <p className="loading-text">Loading...</p>
+                <img src={junimo} className="loading-logo" alt="Loading..." />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default LoadingPage;
