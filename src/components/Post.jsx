c8ba9d0e95a9725857c35d6fcb56aa2c3400c5c4
Kathy Ho <kathyho04@gmail.com>
2024-12-06 23:20:40 -0500

    post comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,13 @@
+/* Post component that displays a single post w/ 
+  details such as title, content, and time posted */
+
 import React from 'react'
 import './Post.css'
 import { Link } from 'react-router-dom'
 import more from '../assets/more.png'
 
 const Post = (props) => {
+  // Calculate time difference btwn when the post was created and the current time
   const timestamp = new Date(props.created_at)
   const currentTime = new Date()
   const diffMinutes = Math.round((currentTime - timestamp) / (1000 * 60))
@@ -12,15 +16,19 @@ const Post = (props) => {
 
   return (
     <div className="Post">
+
+      {/* Edit button linking to the post's edit page */}
       <div>
         <Link to={'edit/' + props.id}>
           <img className="moreButton" alt="edit button" src={more} />
         </Link>
       </div>
 
+      {/* Link to the post's detail page, displays title, content, and upvotes */}
       <Link to={'post/' + props.id} className="postLink">
         <h2 className="name">{props.title}</h2>
         <div className="content">{props.content}</div>
+        {/* Display time since post was made and upvote count */}
         <div className="upvotes">
           {diffMinutes < 60 ? (
             <p>Posted {diffMinutes} minutes ago</p>
